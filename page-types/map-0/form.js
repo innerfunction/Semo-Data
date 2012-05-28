50faf811d3f7478cd08542300b2216c749f47475
Javier Loriente <jloriente@gmail.com>
2012-05-28 15:31:32 +0100

    initial commit

---
+++ page-types/map-0/form.js
@@ -0,0 +1,7 @@
+exports.init = function( $form ) {
+    $.nub.setup( $form );
+    console.log('Setting up google map');
+    // TODO Setup google map server
+};
+exports.validate = function( page ) {
+};
