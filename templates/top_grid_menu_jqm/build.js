aaa11afad61acb902210aadd0d74d43a768e0703
Natalia <natalia.lopez@go-movil.es>
2012-05-30 19:22:22 +0100

    Finish the structure of bottom_grid_menu and top_grid_menu

---
+++ templates/top_grid_menu_jqm/build.js
@@ -0,0 +1,18 @@
+exports.framework = 'jquery-mobile';
+exports.forceBuild = true;
+
+exports.targetForDevice = function( device ) {
+    return 'default';
+};
+
+exports.setup = function( target, device, indir, outdir, callback ) {
+    indir.cp('images css themes js', outdir, callback );
+};
+
+exports.build = function( target, device, data, indir, outdir, page, callback ) {
+    indir.file('index.html').evaluate( data.website, outdir.file('index.html'), callback );
+};
+
+exports.preview = function( device, data, indir, outfile, page, callback ) {
+    indir.file('page.html').evaluate( data, outfile, callback );
+};
