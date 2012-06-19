e45ec0f1c76b815f950d34a913521dd646c07d4e
Natalia <natalia.lopez@go-movil.es>
2012-06-19 16:05:09 +0100

    Natalia test

--- make/test.js
+++ make/test.js
@@ -23,19 +23,19 @@ var website = {
         username : 'username'
     },
     template: {
-        slug: 'single_column_jqm'
+        slug: 'accordion_single_page_jqm'
     },
     theme : {
         id : 'id',
-        slug : 'elevate',
-        base : 'a'
+        slug : 'metro',
+        base : 'c'
     },
     pages: [
         { id: 0, type: 'basic-0', title: 'Page zero', body: 'Soy el cuerpo', topImage: 'http://misteridea.com/wp-content/uploads/misteridea1.png' },
-        { id: 1, type: 'contact-0', title: 'Contact' },
-        { id: 2, type: 'map-0', title: 'Map', tagline: 'this is my site'},
-        { id: 3, type: 'contact-0', title: 'Contact' },
-        { id: 4, type: 'social-media-0', twitter: 'http://twitter.com/natcrea', facebook: 'http://twitter.com/natcrea' }
+        { id: 1, type: 'contact-0', title: 'Contact', address: 'Calle mayor', email: 'a@attch.com', phone:'0852285651', website:'www.innerfuction.com', facebook:'http://facebook.com/misteridea.es', twitter: '@misteridea', linkedin: 'http://linkedin.com/misteridea'},
+        { id: 2, type: 'poll-0', title: 'Poll', question: 'What do you do?'},
+        { id: 3, type: 'contact-form-0', title: 'Contact', address: 'Harcourt Street 20, Dublin 2', twitter: '@twitter', phone: '0875557575'},
+        { id: 4, type: 'two-columns-0', title: 'Two colums', tagLine: 'WE ARE A FRANK AND TENACIOUS PR AGENCY BASED IN DUBLIN, IRELAND THAT PLANS AND EXECUTES INNOVATIVE AND SAVVY CAMPAIGNS FOR A RANGE OF CONSUMER AND CORPORATE CLIENTS.', body:'With 11.7million users and the capacity retain and engage users for the same amount of time as Facebook, Pinterest is the latest social media platform to consume us. But whatâ€™s the best way to use it when promoting a client? And is for big brands at all?', columnOneTitle: 'I am 1', columnTwoTitle:'I am 2', columnOneBody: 'With 11.7million users and the capacity retain and engage users for the same amount of time as Facebook, Pinterest is the latest social media platform to consume us. But whatâ€™s the best way to use it when promoting a client? And is for big brands at all?', columnTwoBody: 'With 11.7million users and the capacity retain and engage users for the same amount of time as Facebook, Pinterest is the latest social media platform to consume us. But whatâ€™s the best way to use it when promoting a client? And is for big brands at all?'  }
     ] 
 };
 
