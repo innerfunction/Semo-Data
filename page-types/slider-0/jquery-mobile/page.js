21d3d411169fc5831d6e0694b728e5ff9c772ecc
Natalia <natalia.lopez@go-movil.es>
2012-07-04 15:56:30 +0100

    Add the new page type 'slider' and add the includes because of the new js in slider

---
+++ page-types/slider-0/jquery-mobile/page.js
@@ -0,0 +1,31 @@
+$(function () {
+
+	// Slideshow 1
+	$("#slider").responsiveSlides({
+		maxwidth: 800,
+		speed: 800
+	});
+	
+	/* Different kind of slider
+	---------------------------  
+	
+	//Slideshow 2: slide with pages
+	$("#slider2").responsiveSlides({
+		auto: false,
+		pager: true,
+		speed: 300,
+		maxwidth: 540
+	});
+	
+	//Slideshow 3: slide with arrows and caption text
+	$("#slider3").responsiveSlides({
+		auto: false,
+		pager: false,
+		nav: true,
+		speed: 500,
+		namespace: "callbacks"
+	});
+	
+	-----------------------------*/
+
+});
