8947e9f8d45436235857a8efb27ff3400da8f79f
jloriente <jloriente@gmail.com>
2012-08-31 14:07:43 +0200

    Changes in preview-edit

--- page-types/basic-0/form.js
+++ page-types/basic-0/form.js
@@ -1,12 +1,28 @@
+//var i18n = require('./lib/i18n');
+
 exports.init = function( $form ) {
     console.log('* Init form.js: Basic-0');
-    console.log( $form );
-    $.nub.init( document.body );
+    console.log( '$FORM JQUERY --> ');
+    console.log($form);
+    //$.nub.init( document.body );
     // TODO: Check why this is not working
     //$.nub.init( $form );
+    $.nub.init();
 };
 
-exports.validate = function( page ) {
+exports.validateDocument = function( page ) {
     console.log('exports.validate form.js basic-0');
+    if (!page.title) {
+        return ('Title is required');
+    }
 };
 
+// rel: /remote/website/data/
+/*exports.validator = function(){
+    'title' : function(val){
+        if (!val) return 'Title is required'
+    },
+    'description' : function(val){
+        if (!description) return 'Description is required'
+    }
+}*/
